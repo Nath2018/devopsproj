93dfe3cc7ac2b6a47db42fbc228d8b21dbdae58a
Nath2018 <38463405+Nath2018@users.noreply.github.com>
2022-02-11 13:57:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "eu-west-2"
+  default = "eu-west-1"
 }
 
 variable "ami_id" {
@@ -7,7 +7,7 @@ variable "ami_id" {
 
   default = {
     #us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    eu-west-1    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
 }
